c09173e672d72b337bb549c4ae5158f36cb75e89
Elisabeth Miller <libbymiiler09@gmail.com>
2018-09-15 12:45:55 -0700

    added html, css, js files

---
+++ index.js
@@ -0,0 +1,5 @@
+//Use jQuery's AJAX functionality to asynchronously load data from one or more APIs
+
+//Use jQuery's DOM traversal and manipulation functionality to display the data from the API to the user in an interesting way
+
+//Use jQuery's event listening capabilities to respond to user actions
