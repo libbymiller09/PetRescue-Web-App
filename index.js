24a8910f764015c3531b7051541fe202fc334de7
Elisabeth Miller <libbymiiler09@gmail.com>
2018-09-17 10:32:16 -0700

    added api request functions to index.js and added some image and styling changes

--- index.js
+++ index.js
@@ -1,5 +1,48 @@
-//Use jQuery's AJAX functionality to asynchronously load data from one or more APIs
+//API for google maps = AIzaSyB-FEEnkqYHiCzepPgtDMj_0hPfeignmM4
 
-//Use jQuery's DOM traversal and manipulation functionality to display the data from the API to the user in an interesting way
+//API for rescue pets = n8EpA93N
 
-//Use jQuery's event listening capabilities to respond to user actions
+
+const google_maps_url = 'https://maps.googleapis.com/maps/api/js?key=';
+const rescue_pets_url = 'http://www.';
+
+//function to access data from the user's search 
+function getDataFromApi(searchTerm, callback) {
+    const query = {
+        part: 'snippet',
+        key: 'n8EpA93N',
+        q: `${searchTerm} in:name`
+    }
+    $.getJSON(rescue_pets_url, query, callback);
+}
+
+//function to generate the results page
+function renderResults(result) {
+    return `
+    <div class="results">
+      <p>Great News! We've matched you with some awesome available pets!</p>
+      <button type="button" id="adoptMe">Adopt Me!</button>
+      <a href="${result.id}"> 
+      <img src="${result}" alt="available pet image">
+    </div>`
+}
+
+//function that generates the renderResults function when needed to display data
+function displayRescueDatat(data) {
+  const results = data.items.map((item, index) => renderResults(item));
+  $('.js-results').html(results);
+}
+
+//function that watches for the search now button to be clicked and runs the callback function
+function watchSubmit() {
+  $('.searchForm').submit(function (event) {
+    event.preventDefault();
+    const queryTarget = $(event.currentTarget).find('.js-query');
+    const query = queryTarget.val();
+    // clear out the input
+    queryTarget.val("");
+    getDataFromApi(query, displayYouTubeSearchData);
+  });
+}
+
+
