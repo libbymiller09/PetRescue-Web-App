0ef757bd570eb3afe5245b95c8577494cfb6b6da
Elisabeth Miller <libbymiiler09@gmail.com>
2018-09-18 17:43:49 -0700

    styling carousel component more

--- index.js
+++ index.js
@@ -3,22 +3,20 @@
 //API for rescue pets = n8EpA93N
 
 const google_maps_url = 'https://maps.googleapis.com/maps/api/js?key=';
-const rescue_pets_url ='https://api.rescuegroups.org/v5/public/animals?start='
+const pet_finder_url ="http://api.petfinder.com/my.method?key="
 
 //function to access Rescue data from the user's search 
 function getDataFromRescueApi(searchTerm, callback) {
     const query = {
-      // part: 'snippet',
-      apikey: 'n8EpA93N',
+      apikey: '3fd075241e863aa486d764451a6a094d',
       q: `${searchTerm} in:name`
     }
-    $.getJSON(rescue_pets_url, query, callback);
+    $.getJSON(pet_finder_url, query, callback);
 }
 
-//function to access google maps data from user's search
+// function to access google maps data from user's search
 function getDataFromMapsApi(searchTerm, callback) {
   const query = {
-    part: 'snippet',
     key: 'AIzaSyB-FEEnkqYHiCzepPgtDMj_0hPfeignmM4',
     q: `${searchTerm} in:name`
   }
@@ -49,6 +47,20 @@ function displayRescueData(data) {
   const results = data.items.map((item, index) => renderResults(item));
   $('.js-results').html(results);
 }
+
+//function that generates the map data
+function displayMapData() {
+  let portland = {lat: 45.5122, lng: -122.6587};
+  // The map, centered at Portland
+  let map = new google.maps.Map(
+      document.getElementById('map'), {zoom: 12, center: portland});
+  // The marker, positioned at Portland
+  let marker = new google.maps.Marker({position: portland, map: map});
+  renderMapResults(item);
+  $('.js-map').html(results);
+}
+
+displayMapData();
 
 //function that watches for the search now button to be clicked and runs the callback function
 function watchSubmit() {
