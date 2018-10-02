adabc43c59ad5a5b010edddb533495c41caae1da
Elisabeth Miller <libbymiiler09@gmail.com>
2018-10-01 22:51:54 -0700

    finished functionality and added slider tech

--- index.js
+++ index.js
@@ -1,213 +1,276 @@
-const apiKey = 'AIzaSyCPkdxPck9_Ifk7N417g8v3e4DKJQxxFUc';
+const apiKey = "AIzaSyCPkdxPck9_Ifk7N417g8v3e4DKJQxxFUc";
 // const google_maps_url = `https://maps.googleapis.com/maps/api/js?key=${apiKey}`;
 const google_maps_url = `http://maps.googleapis.com/maps/api/geocode/json?address=83%20Decathlon%20Circle%20Sacramento&key=${apiKey}`;
-const pet_finder_url = 'https://api.petfinder.com/pet.find';
-const pet_finder_shelter_url = 'https://api.petfinder.com/shelter.find';
+const pet_finder_url = "https://api.petfinder.com/";
+const pet_finder_shelter_url = "https://api.petfinder.com/shelter.find";
 
 //function that retrieves the shelter information from the petfinder api
 function getShelterData(query) {
-    $.ajax({
-        url: pet_finder_shelter_url,
-        method: 'GET',
-        dataType: 'jsonp',
-        crossDomain: true,
-        data: {
-            key: '3fd075241e863aa486d764451a6a094d',
-            location: query.zip,
-            format: 'json'
-        },   
-        success: (response) => handleShelterInfo(response)  
-    });
+  return $.ajax({
+    url: pet_finder_shelter_url,
+    method: "GET",
+    dataType: "jsonp",
+    crossDomain: true,
+    data: {
+      key: "3fd075241e863aa486d764451a6a094d",
+      location: query.zip,
+      format: "json"
+    },
+    success: response => handleShelterInfo(response)
+  });
 }
 
 //function that handles shelter results
 function handleShelterInfo(response) {
-    if (response) {
-
-    }
-    console.log(response.petfinder.shelters.shelter.city);
-    response.petfinder.shelters.shelter.forEach((shelter) => {
-        
-        // findLatLng(response);
-        // addShelterToMap(response, location);
-        addShelterToList({
-            name: shelter.name,
-            city: shelter.city,
-            longitude: shelter.longitude,
-            latitude: shelter.latitude,
-        });
-        geocodeLatLng(response);
-    })
-}
-
-//function to add shelter cordinates marker to map --needs to loop through the shelters and put each on the map
-// function addShelterToMap(response, location, map) { 
-//     response.petfinder.shelters.shelter.forEach((shelter) => {
-//         let latlng = {
-//             lat = response.petfinder.shelters.shelter.latitude,
-//             lng = response.petfinder.shelters.shelter.longitude
-//         }
-//         // let latlng = findLatLng(response);
-//         let marker = new google.maps.Marker({
-//             position: latlng,
-//             map: map
-//         });
-//     })    
-// }
-
-//function to add shelter to list
-function addShelterToList(shelter) {
-    let newDiv = document.createElement('div');
-    let newName = document.createElement('p');
-    let newCity = document.createElement('p');
-    let newLat = document.createElement('p');
-    let newLong = document.createElement('p');
-    
-    newName.textContent = shelter.name.$t;
-    newCity.textContent = shelter.city.$t;
-    newLat.textContent = shelter.latitude.$t;
-    newLong.textContent = shelter.longitude.$t;
-        
-    newDiv.appendChild(newName);
-    newDiv.appendChild(newCity);
-    newDiv.appendChild(newLat);
-    newDiv.appendChild(newLong);
-
-    document.querySelector('#shelter-results').appendChild(newDiv);
-}
-
-//function to access Rescue data from the user's search 
-function getDataFromPetFinderApi(query) {
-    $.ajax({
-        url: pet_finder_url,
-        method: 'GET',
-        dataType: 'jsonp',
-        crossDomain: true,
-        data: {
-            key: '3fd075241e863aa486d764451a6a094d',
-            animal: query.typeOfPet,
-            location: query.zip,
-            output: 'basic',
-            format: 'json'
-        },
-        success: (response) => handlePetsFindResult(response)
+  if (response) {
+  }
+  response.petfinder.shelters &&
+    response.petfinder.shelters.shelter.forEach(shelter => {
+      addShelterToMap({
+        latitude: shelter.latitude.$t,
+        longitude: shelter.longitude.$t,
+        name: shelter.name.$t,
+        id: shelter.id.$t
+      });
+      addShelterToList({
+        name: shelter.name,
+        city: shelter.city,
+        longitude: shelter.longitude,
+        latitude: shelter.latitude,
+        id: shelter.id
+      });
     });
 }
 
+//function to add shelter to list
+function addShelterToList(shelter) {
+  let newDiv = document.createElement("div");
+  let newName = document.createElement("p");
+  let newCity = document.createElement("p");
+  let newLat = document.createElement("p");
+  let newLong = document.createElement("p");
+
+  newName.textContent = shelter.name.$t;
+  newCity.textContent = shelter.city.$t;
+  newLat.textContent = shelter.latitude.$t;
+  newLong.textContent = shelter.longitude.$t;
+
+  newDiv.appendChild(newName);
+  newDiv.appendChild(newCity);
+  newDiv.appendChild(newLat);
+  newDiv.appendChild(newLong);
+
+  newDiv.setAttribute("data-shelter-id", shelter.id.$t);
+
+  document.querySelector("#shelter-results").appendChild(newDiv);
+}
+
+//function to access Rescue data from the user's search
+function getDataFromPetFinderApi(query) {
+  const data = {
+    key: "3fd075241e863aa486d764451a6a094d",
+    output: "basic",
+    format: "json"
+  };
+  let method;
+
+  if (query.shelterId) {
+    data.id = query.shelterId;
+
+    method = "shelter.getPets";
+  } else {
+    data.location = query.zip;
+    data.animal = query.typeOfPet;
+
+    method = "pet.find";
+  }
+
+  return $.ajax({
+    url: pet_finder_url + method,
+    method: "GET",
+    dataType: "jsonp",
+    crossDomain: true,
+    data: data,
+    success: response => handlePetsFindResult(response)
+  });
+}
+
 function handlePetsFindResult(response) {
-    response.petfinder.pets && response.petfinder.pets.pet.forEach((pet) => {
-        addPetToList({
-            id: pet.id.$t,
-            name: pet.name.$t,
-            img: pet.media.photos && pet.media.photos.photo[0] && pet.media.photos.photo[0].$t,
-        });
+  response.petfinder.pets &&
+    response.petfinder.pets.pet &&
+    response.petfinder.pets.pet.forEach(pet => {
+      addPetToList({
+        id: pet.id.$t,
+        name: pet.name.$t,
+        img:
+          pet.media.photos &&
+          pet.media.photos.photo[0] &&
+          pet.media.photos.photo[0].$t,
+        shelterId: pet.shelterId.$t
+      });
     });
 }
 
 function addPetToList(pet) {
-    let newDiv = document.createElement('div');
-    let newName = document.createElement('a');
+  let newDiv = document.createElement("div");
+  let newName = document.createElement("a");
 
-    newName.textContent = pet.name;
-    newName.href = 'https://www.petfinder.com/petdetail/' + pet.id;
+  newName.textContent = pet.name;
+  newName.href = "https://www.petfinder.com/petdetail/" + pet.id;
 
-    if (pet.img) { 
-        let newImg = document.createElement('img');
-        newImg.src = pet.img;
+  if (pet.img) {
+    let newImg = document.createElement("img");
+    newImg.src = pet.img;
 
-        newImg.onerror = () => {
-            newImg.parentElement.removeChild(newImg);
-        };
+    newImg.onerror = () => {
+      newImg.parentElement.removeChild(newImg);
+    };
 
-        newDiv.appendChild(newImg);
-    }
+    newDiv.appendChild(newImg);
+  }
+  newDiv.setAttribute("data-shelter-id", pet.shelterId);
 
-    newDiv.appendChild(newName);
-    
-    document.querySelector('.results').appendChild(newDiv);
+  newDiv.appendChild(newName);
+
+  document.querySelector(".results").appendChild(newDiv);
 }
 
 function resetForm() {
-    console.log('TO DO!!! WRITE THIS FUNCTION')
-    // let query = queryTarget.value(""); 
+  console.log("TO DO!!! WRITE THIS FUNCTION");
+  // let query = queryTarget.value("");
 }
 
 function resetResultsList() {
-    console.log('TODO !!!! WRITE THIS FUNCTION!')
-    // document.querySelector('.js-query').value("");
+  document.querySelector(".results").innerHTML = "";
+  document.querySelector("#shelter-results").innerHTML = "";
 }
 
-//   function that initializes and generates the map 
+//   function that initializes and generates the map
 function initMap() {
-    let portland = {
-        lat: 45.5122, 
-        lng: -122.6587,
-    };
+  let portland = {
+    lat: 45.5122,
+    lng: -122.6587
+  };
 
-    // The map, centered at Portland
-    let map = new google.maps.Map(
-        document.getElementById("js-map"), 
-        {
-            zoom: 12, 
-            center: portland
-        }
-    );
-    var geocoder = new google.maps.Geocoder;
-    var infowindow = new google.maps.InfoWindow;
+  // The map, centered at Portland
+  window.map = new google.maps.Map(document.getElementById("js-map"), {
+    zoom: 12,
+    center: portland
+  });
 
-    document.getElementById('searchButton').addEventListener('click', function() {
-        geocodeLatLng(geocoder, map, infowindow);
-    });
-}   
+  // window.geocoder = new google.maps.Geocoder;
+  // window.infowindow = new google.maps.InfoWindow;
 
+  // document.getElementById('searchButton').addEventListener('click', function() {
+  //     geocodeLatLng(geocoder, map, infowindow);
+  // });
+}
 
-function geocodeLatLng(response, geocoder, map, infowindow) {
-    //need to match the value of latlng from geocode to petfinder value lat lng
-    // var input = document.getElementById('latlng').value;
-   var latlng = {
-       lat: response.petfinder.shelters.shelter.latitude.$t,
-       lng: response.petfinder.shelters.shelter.longitude.$t
-   }
-   var geocoder = new google.maps.Geocoder;
+//function that matches latitude and longitude from petfinder api to geocode maps api and places a marker on the map
+function addShelterToMap(shelter) {
+  var marker = new google.maps.Marker({
+    position: {
+      lat: parseFloat(shelter.latitude),
+      lng: parseFloat(shelter.longitude)
+    }
+  });
 
-    geocoder.geocode({'location': latlng}, function(results, status) {
-      if (status === 'OK') {
-        if (results[0]) {
-          map.setZoom(11);
-             // Place Location Marker
-          var marker = new google.maps.Marker({
-            position: latlng,
-            map: map
-          });
-          infowindow.setContent(results[0].formatted_address);
-          infowindow.open(map, marker);
-        } else {
-          window.alert('No results found');
-        }
+  marker.setMap(map);
+
+  const infowindow = new google.maps.InfoWindow();
+
+  infowindow.setContent(shelter.name);
+  infowindow.open(map, marker);
+
+  marker.addListener("click", function(event) {
+    highlightAnimalsInShelter(shelter);
+  });
+}
+
+function highlightAnimalsInShelter(shelter) {
+  document
+    .querySelectorAll("[data-shelter-id]")
+    .forEach(function(shelterElement) {
+      if (shelterElement.getAttribute("data-shelter-id") !== shelter.id) {
+        shelterElement.classList.add("darken");
       } else {
-        window.alert('Geocoder failed due to: ' + status);
+        shelterElement.classList.remove("darken");
       }
     });
-  }
+
+  resetResultsList();
+  getDataFromPetFinderApi({
+    shelterId: shelter.id
+  }).then(function() {
+    openShelterResults();
+  });
+}
 
 //function that watches for the search now button to be clicked and runs the callback function
 function watchSubmit() {
-  $('.searchForm').submit(function (event) {
+  $(".searchForm").submit(function(event) {
     event.preventDefault();
-    
-    const query = {
-        typeOfPet: event.currentTarget.querySelector('#typeOfPet').value || 'dog',
-        zip: event.currentTarget.querySelector('#zip').value || '97217',
+
+    window.query = {
+      typeOfPet: event.currentTarget.querySelector("#typeOfPet").value || "dog",
+      zip: event.currentTarget.querySelector("#zip").value || "97217"
     };
 
     resetResultsList();
-    getShelterData(query);
-    getDataFromPetFinderApi(query);
+
+    Promise.all([getShelterData(query), getDataFromPetFinderApi(query)]).then(
+      function() {
+        closeSearchForm();
+        openShelterResults();
+      }
+    );
     resetForm();
   });
 }
 
-$(watchSubmit);
+function watchMenuButtons() {
+  document
+    .querySelector(".open-search-form-button")
+    .addEventListener("click", function() {
+      openSearchForm();
+    });
 
-	
+  document
+    .querySelector(".close-search-form-button")
+    .addEventListener("click", function() {
+      closeSearchForm();
+    });
 
+  document
+    .querySelector(".open-results-panel-button")
+    .addEventListener("click", function() {
+      openShelterResults();
+    });
+
+  document
+    .querySelector(".close-results-panel-button")
+    .addEventListener("click", function() {
+      closeShelterResults();
+    });
+}
+
+function openSearchForm() {
+  document.querySelector("body").classList.add("search-form-open");
+}
+
+function closeSearchForm() {
+  document.querySelector("body").classList.remove("search-form-open");
+}
+
+function openShelterResults() {
+  document.querySelector("body").classList.add("results-panel-open");
+}
+
+function closeShelterResults() {
+  document.querySelector("body").classList.remove("results-panel-open");
+}
+
+$(function() {
+  watchSubmit();
+  watchMenuButtons();
+});
